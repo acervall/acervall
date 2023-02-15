9908c643b81a2646fb87ac3bcff09a632f8453ad
acervall <ameliecervall@gmail.com>
2023-02-15 20:40:43 +0100

    add avatars

--- src/views/FunktionerView.vue
+++ src/views/FunktionerView.vue
@@ -1,5 +1,14 @@
 <template>
   <MerFunktioner @emitvaluex="customeventfunktion" propvalue="hej" />
+  <img src="/assets/avatar/avatar1.jpg" />
+  <img src="/assets/avatar/avatar9.jpg" />
+  <img src="/assets/avatar/avatar8.jpg" />
+  <img src="/assets/avatar/avatar7.jpg" />
+  <img src="/assets/avatar/avatar6.jpg" />
+  <img src="/assets/avatar/avatar5.jpg" />
+  <img src="/assets/avatar/avatar4.jpg" />
+  <img src="/assets/avatar/avatar3.jpg" />
+  <img src="/assets/avatar/avatar2.jpg" />
 </template>
 
 <script>
